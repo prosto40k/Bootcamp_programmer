bd50a6504d310ee04cd19758f55f9c68f7f8d2e0
bloody <novickiy.n.l@gmail.com>
2023-02-04 18:38:14 +0300

    Lesson27.01

---
+++ lesson_27_Basics_of_creating_a_telegram_bot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+
+
+//JSON-служит для передачи данных между клиентом и сервером
+// {
+//     "ok":true,
+//     "result":
+//     {
+//         "id":6142681683,
+//         "is_bot":true,
+//         "first_name":"\u041c\u043e\u0439 \u043f\u0435\u0440\u0432\u044b\u0439 \u0431\u043e\u0442",
+//         "username":"FirstTest01_bot",
+//         "can_join_groups":true,
+//         "can_read_all_group_messages":false,
+//         "supports_inline_queries":false
+//     }
+// }
+
+// class GetMeModel
+// {
+//     bool ok;
+//     ResultModel result;
+// }
+
+// class ResultModel
+// {
+//     long id;
+//     bool is_bot;
+//     string first_name;
+//     string username;
+// }
+
+// Пишем парсер(нечто, Преобразующее(что то не понятное для нас) {JSON} во что то понятное нам) 
+//для сложного объекта
+// {
+//     "наименование флага":значение флага,
+// }
+
+string token = File.ReadAllText("token.config");
+HttpClient hc = new();
+hc.BaseAddress = new Uri($"https://api.telegram.org/bot{token}/");
+
+string contentObj = hc.GetStringAsync("getme").Result;
+System.Console.WriteLine(contentObj);
+31:38
